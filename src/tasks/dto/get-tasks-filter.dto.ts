fec3cef9f15b5c2381a06b04c7e41e3c5fea06f4
Lewis_Damy <ldamy16@gmail.com>
2024-07-07 15:21:01 -0300

    ✨ Added dto for tasks module

---
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -0,0 +1,11 @@
+import { IsEnum, IsOptional } from 'class-validator';
+import { TaskStatus } from '../task-status.enum';
+
+export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
+  status?: TaskStatus;
+
+  @IsOptional()
+  search?: string;
+}
