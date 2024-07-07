e86de74ec59e1630cd9c05d754aa2f969b2e05e4
Lewis_Damy <ldamy16@gmail.com>
2024-07-07 16:28:09 -0300

    ✨ Added minimum requirements for creating user and password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,18 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  //   https://gist.github.com/arielweinberger/18a29bfa17072444d45adaeeb8e92ddc
+  // Password to contain at least: 1 uppercase, 1 lowercase, 1 number or special char
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
   password: string;
 }
